937d8e18c11fd34ff03d1c357c8cda338e4344f3
Dag Stuan <D.Stuan@gmail.com>
2018-05-01 15:30:46 +0200

    Use logo exported from illustrator

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Img from 'gatsby-image';
 
-import logo from '../assets/logo.svg';
+import logo from '../assets/logosvg.svg';
 
 const IndexPage = ({ data }) => {
   const {
