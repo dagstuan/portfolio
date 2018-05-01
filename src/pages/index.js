d9e0f1393ac97cf0b25f270e44476b34aca6e04b
Dag Stuan <D.Stuan@gmail.com>
2018-05-01 15:32:47 +0200

    oops

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Img from 'gatsby-image';
 
-import logo from '../assets/logosvg.svg';
+import logo from '../assets/logo.svg';
 
 const IndexPage = ({ data }) => {
   const {
