a874f66c61ecd9196c10b61b152d9b76d1d37023
Dag Stuan <D.Stuan@gmail.com>
2018-05-01 16:41:25 +0200

    Add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,12 @@ module.exports = {
           '6a3d76351127260438e7129b6981988752127f3b28c7da5f12b428f414527b0f',
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-35576492-1',
+        anonymize: false,
+      },
+    },
   ],
 };
