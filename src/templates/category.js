bbc20bab7910f00b11d7b52529dbce73e3e67305
Dag Stuan <d.stuan@gmail.com>
2018-11-23 15:29:17 +0100

    Better prefetching of images

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,141 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 import Helmet from 'react-helmet';
 
 import { imageMetaTags, titleMetaTags } from '../utils/metaUtils';
 
-class CategoryElem extends React.Component {
-  constructor() {
-    super();
-    this.containerRef = null;
+import CategoryImage from './categoryImage';
+
+class Category extends Component {
+  constructor(props) {
+    super(props);
 
     this.state = {
-      imageWidth: 0,
-      imageHeight: 0,
-      containerWidth: 0,
-      containerHeight: 0,
+      loadedImages: [],
     };
   }
 
-  setContainerRef = el => (this.containerRef = el);
+  onImageLoad = id => {
+    const { loadedImages } = this.state;
 
-  componentDidMount() {
-    this.updateDimensions();
-    window.addEventListener('resize', this.updateDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions);
-  }
-
-  getImageAspectRatio = () => {
-    const { image } = this.props;
-
-    const imageWidth = Math.min(
-      image.image.fixed.width,
-      image.image.file.details.image.width
-    );
-    const imageHeight = imageWidth / image.image.fixed.aspectRatio;
-
-    return imageWidth / imageHeight;
-  };
-
-  updateDimensions = () => {
-    const { containerWidth, containerHeight } = this.state;
-
-    const newContainerWidth = this.containerRef.offsetWidth;
-    const newContainerHeight = this.containerRef.offsetHeight;
-
-    if (
-      containerWidth === newContainerWidth &&
-      containerHeight === newContainerHeight
-    ) {
-      return;
+    if (!loadedImages.includes(id)) {
+      loadedImages.push(id);
     }
 
-    const aspectRatio = this.getImageAspectRatio();
-
-    const style = {
-      containerWidth: newContainerWidth,
-      containerHeight: newContainerHeight,
-    };
-
-    if (newContainerWidth / newContainerHeight > aspectRatio) {
-      style.imageHeight = newContainerHeight + 'px';
-      style.imageWidth = (newContainerHeight * aspectRatio).toString() + 'px';
-    } else {
-      style.imageWidth = newContainerWidth + 'px';
-      style.imageHeight = (newContainerWidth / aspectRatio).toString() + 'px';
-    }
-
-    this.setState(style);
+    this.setState({
+      loadedImages,
+    });
   };
 
   render() {
-    const { image } = this.props;
+    const {
+      data: {
+        contentfulCategory: { title, images },
+      },
+    } = this.props;
 
-    const { imageWidth, imageHeight } = this.state;
+    const { loadedImages } = this.state;
+
+    const firstImage = images[0];
+
+    const {
+      title: imageTitle,
+      image: { resize },
+    } = firstImage;
 
     return (
-      <li className="category-elem">
-        <div
-          className="category-elem__image-outer-wrapper"
-          ref={this.setContainerRef}
-        >
-          <div
-            className="category-elem__image-wrapper"
-            style={{
-              width: imageWidth,
-              height: imageHeight,
-            }}
-          >
-            <Img
-              fixed={image.image.fixed}
-              alt={image.title}
-              className="category-elem__image"
-              style={{ width: '100%', height: '100%' }}
-            />
-          </div>
+      <>
+        <Helmet>
+          <title>{title} - Dag Stuan</title>
+          {titleMetaTags(`${title} - Dag Stuan`)}
+          {imageMetaTags(resize, imageTitle)}
+        </Helmet>
+        <div className="title__wrapper">
+          <h1 className="title">{title}</h1>
         </div>
-      </li>
+
+        {images && (
+          <ul className="category-elems">
+            {images.map(image => {
+              const anyLoaded = loadedImages.length > 0;
+              const isLoaded = loadedImages.includes(image.id);
+
+              return (
+                <CategoryImage
+                  key={image.id}
+                  image={image}
+                  critical={anyLoaded && !isLoaded}
+                  onImageLoad={() => this.onImageLoad(image.id)}
+                />
+              );
+            })}
+          </ul>
+        )}
+      </>
     );
   }
 }
-
-const Category = ({ data }) => {
-  const {
-    contentfulCategory: { title, images },
-  } = data;
-
-  const firstImage = images[0];
-
-  const {
-    title: imageTitle,
-    image: { resize },
-  } = firstImage;
-
-  return (
-    <>
-      <Helmet>
-        <title>{title} - Dag Stuan</title>
-        {titleMetaTags(`${title} - Dag Stuan`)}
-        {imageMetaTags(resize, imageTitle)}
-      </Helmet>
-      <div className="title__wrapper">
-        <h1 className="title">{title}</h1>
-      </div>
-
-      {images && (
-        <ul className="category-elems">
-          {images.map((image, index) => (
-            <CategoryElem key={index} image={image} />
-          ))}
-        </ul>
-      )}
-    </>
-  );
-};
 
 export const query = graphql`
   query PostQuery($slug: String!) {
@@ -143,6 +82,7 @@ export const query = graphql`
       title
       slug
       images {
+        id
         title
         image {
           resize(width: 1200) {
